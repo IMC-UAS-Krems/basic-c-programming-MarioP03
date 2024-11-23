2be0255bfa9b4bf7e666ac3d3cd299f248a8ad53
Mario <23imc11294@fh-krems.ac.at>
2024-11-23 23:55:09 +0100

    Ex 1 and 2 Done

--- assignment.c
+++ assignment.c
@@ -12,7 +12,18 @@ int main(int argc, char *argv[]) {
     int maxrand = 100;
 
     // WRITE YOUR CODE HERE
-    
+    int n = argc-1;
+    if (argc != 3) {
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\n", n);
+        exit(EXIT_FAILURE); // so the program doesn't write 'Segmentation fault' for trying to continue with the other stuff
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    if (a <= 0 || b <= 0) {
+        printf("Incorrect usage. The parameters you provided are not positive integers\n");
+    }
+
 
     return 0;
 }
